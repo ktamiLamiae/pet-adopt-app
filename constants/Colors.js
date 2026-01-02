8fb94176201d50256d8312e02087bf03b9519e6f
loubna-ait <loubnaaithra@gmail.com>
2026-01-02 13:01:03 +0100

    add colors

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,5 +1,9 @@
 export default {
     PRIMARY: "#E8B20E",
+    LIGHT_PRIMARY: "#fff1c9",
     GRAY: "#8F8e8d",
-    WHITE: "#fff"
+    WHITE: "#fff",
+    SECONDARY: "#ffc573ca",
+    RED: "#FF0000",
+    BLACK: "#000"
 }
